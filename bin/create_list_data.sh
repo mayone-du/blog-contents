0fa6fe1430df995af97b85c9f924f5bfc29fcfbe
石川優輝 <ishikawayuuki@localadmins-MacBook-Pro-m0041.local>
2022-01-05 14:49:12 +0900

    update: shell script

--- bin/create_list_data.sh
+++ bin/create_list_data.sh
@@ -2,15 +2,19 @@
 ARTICLE_LIST=($(ls ./articles))
 TITLE_KEY_AND_VALUES=()
 DESCRIPTION_KEY_AND_VALUES=()
+INDEX=0
+JSON="["
 for ARTICLE in ${ARTICLE_LIST[@]}; do
+  let INDEX++
   ARTICLE_PATH="./articles/${ARTICLE}"
   TITLE=$(grep -m 1 "title: " ${ARTICLE_PATH} | sed -e "s/title: //g")
   DESCRIPTION=$(grep -m 1 "description: " ${ARTICLE_PATH} | sed -e "s/description: //g")
-  TITLE_KEY_AND_VALUES+="\"title\": \"${TITLE}\","
-  DESCRIPTION_KEY_AND_VALUES+="\"description\": \"${DESCRIPTION}\","
+  JSON+="{\"title\": \"${TITLE}\", \"description\": \"${DESCRIPTION}\"}"
+  # 最後のループじゃなければコンマを追加
+  if [ ${#ARTICLE_LIST[@]} != $INDEX ]; then
+    JSON+=","
+  fi
 done
+JSON+="]"
 
-echo '{
-  "title": "'$TITLE_KEY_AND_VALUES'",
-  "description": "'$DESCRIPTION_KEY_AND_VALUES'",
-}'
+echo $JSON
