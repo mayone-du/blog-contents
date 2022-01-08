cec9c6309284b6a52b67a5f19d833f405478ba4d
mayone-du <cocomayo1201@icloud.com>
2022-01-08 12:01:09 +0900

    fix: actions

--- bin/git.sh
+++ bin/git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add -N .
-if [ ! git diff --exit-code --quiet ]; then
+if ! git diff --exit-code --quiet; then
   git config user.name github-actions
   git config user.email github-actions@github.com
   git add .
