9a951d2209fd73629f4f1d9b661ce36c2f1c1002
mayone-du <cocomayo1201@icloud.com>
2022-01-10 13:19:17 +0900

    fix

--- bin/create_other_list.sh
+++ bin/create_other_list.sh
@@ -2,6 +2,7 @@
 ARTICLE_LIST=($(ls -R ./others | grep .md$))
 META_ATTRIBUTES=(title description emoji is_published)
 FILE_INDEX=0
+ATTRIBUTES_INDEX=0
 JSON="["
 for ARTICLE in ${ARTICLE_LIST[@]}; do
   let FILE_INDEX++
@@ -9,7 +10,7 @@ for ARTICLE in ${ARTICLE_LIST[@]}; do
 
   JSON+="{"
   for ATTRIBUTES in ${META_ATTRIBUTES[@]}; do
-    ATTRIBUTES_INDEX=0
+    let ATTRIBUTES_INDEX++
     VALUE=$(grep -m 1 "${ATTRIBUTES}: " ${ARTICLE_PATH}| sed -e "s/${ATTRIBUTES}: //g")
     if [ "${ATTRIBUTES}" == "is_published" ]; then
       JSON+="\"${ATTRIBUTES}\": ${VALUE}"
@@ -27,6 +28,8 @@ for ARTICLE in ${ARTICLE_LIST[@]}; do
   if [ ${#ARTICLE_LIST[@]} != $FILE_INDEX ]; then
     JSON+=","
   fi
+
+  let ATTRIBUTES_INDEX=0
 
 done
 JSON+="]"
