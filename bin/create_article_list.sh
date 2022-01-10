ff881caf68d91b597e8af287ff346c2a93628631
mayone-du <cocomayo1201@icloud.com>
2022-01-10 12:19:47 +0900

    fix: shell scripts

--- bin/create_article_list.sh
+++ bin/create_article_list.sh
@@ -10,18 +10,18 @@ for ARTICLE in ${ARTICLE_LIST[@]}; do
 
   JSON+="{"
   for ATTRIBUTES in ${META_ATTRIBUTES[@]}; do
-    # ATTRIBUTES_INDEX=0
+    ATTRIBUTES_INDEX=0
     VALUE=$(grep -m 1 "${ATTRIBUTES}: " ${ARTICLE_PATH} | sed -e "s/${ATTRIBUTES}: //g")
     if [ "${ATTRIBUTES}" == "is_published" ]; then
-      JSON+="\"${ATTRIBUTES}\": ${VALUE},"
+      JSON+="\"${ATTRIBUTES}\": ${VALUE}"
     else
-      JSON+="\"${ATTRIBUTES}\": \"${VALUE}\","
+      JSON+="\"${ATTRIBUTES}\": \"${VALUE}\""
     fi
 
     # 最後のメタ属性のループでなければ、カンマを追加する
-    # if [ ${#META_ATTRIBUTES[@]} != $ATTRIBUTES_INDEX ]; then
-    #   JSON+=","
-    # fi
+    if [ ${#META_ATTRIBUTES[@]} != $ATTRIBUTES_INDEX ]; then
+      JSON+=","
+    fi
   done
 
   JSON+="}"
