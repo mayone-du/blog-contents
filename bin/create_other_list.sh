364b8bc7d2d85ac25598f36dad0d30e804013f51
mayone-du <cocomayo1201@icloud.com>
2022-01-10 12:17:37 +0900

    fix: shell scripts

--- bin/create_other_list.sh
+++ bin/create_other_list.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 ARTICLE_LIST=($(ls -R ./others | grep .md$))
 META_ATTRIBUTES=(title description emoji is_published)
-INDEX=0
+FILE_INDEX=0
 JSON="["
 for ARTICLE in ${ARTICLE_LIST[@]}; do
-  let INDEX++
+  let FILE_INDEX++
   ARTICLE_PATH="./others/${ARTICLE}"
 
   JSON+="{"
   for ATTRIBUTES in ${META_ATTRIBUTES[@]}; do
-    JSON+=$(grep -m 1 "${ATTRIBUTES}: " ${ARTICLE_PATH} | sed -e "s/${ATTRIBUTES}: //g")
+    ATTRIBUTES_INDEX=0
+    ATTRIBUTES_INDEX++
+    VALUE=$(grep -m 1 "${ATTRIBUTES}: " ${ARTICLE_PATH}| sed -e "s/${ATTRIBUTES}: //g")
+    if [ "${ATTRIBUTES}" == "is_published" ]; then
+      JSON+="\"${ATTRIBUTES}\": ${VALUE}"
+    else
+      JSON+="\"${ATTRIBUTES}\": \"${VALUE}\""
+    fi
+
+    # 最後のメタ属性のループでなければ、カンマを追加する
+    if [ ${#META_ATTRIBUTES[@]} != $ATTRIBUTES_INDEX ]; then
+      JSON+=","
+    fi
   done
   JSON+="}"
 
-  if [ ${#ARTICLE_LIST[@]} != $INDEX ]; then
+  if [ ${#ARTICLE_LIST[@]} != $FILE_INDEX ]; then
     JSON+=","
   fi
 
